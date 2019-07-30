56063802c10538885b454db987f055b0632b9f47
sx <sx@xblz.org>
2019-07-30 23:40:40 +0800

    update

--- pages/vision/vision.js
+++ pages/vision/vision.js
@@ -1,66 +1,43 @@
-// pages/vision/vision.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    width: 0,
+    step: 0,  // 步骤
+    level: 1, // 等级
+    count: 9,  // 方块数量
+    live: 3,  // 错误次数
+    arr: [],  // 问题
+    result: []// 结果
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
+  onLoad(options) {
+    const { level} = this.data;
+    this.setData({ width: (wx.getSystemInfoSync().windowWidth - 60) / (level + 2) -  8 })
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage() {
 
+  },
+
+  onClickStart() {
+    this.setData({ step: 1 })
   }
 })
+// 等级 方格 块数
+// 1 3 3
+// 2 3 4
+// 3 4 5
+// 4 4 6
+// 5 4 7
+// 6 5 8
+// 7 6 9
+// 8 7 10
